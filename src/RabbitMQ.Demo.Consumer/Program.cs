54a6ba9cfec97681b1a159aa70669cd384e384b7
Billy Ngo <thanhdat.hitech@gmail.com>
2021-08-14 19:41:39 +0700

    Add project files.

---
+++ src/RabbitMQ.Demo.Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.Demo.Consumer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("BasicQueue", false, false, false, null);
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+                        Console.WriteLine(message);
+                        channel.BasicAck(ea.DeliveryTag, false);
+
+                    };
+                    channel.BasicConsume("BasicQueue", false, consumer);
+                    Console.ReadLine();
+                }
+            }
+
+        }
+
+    }
+}
---
+++ src/RabbitMQ.Demo.Producer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using RabbitMQ;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Demo.Producer
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() {HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("BasicQueue", false, false, false, null);
+                    while (true)
+                    {
+                        var message = Console.ReadLine();
+                        var body = Encoding.UTF8.GetBytes($"Billy: {message}");
+                        channel.BasicPublish("", "BasicQueue", null, body);
+                    }
+                }
+            }
+        }
+    }
+}
